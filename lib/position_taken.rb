9a2ab83d0e9c901829e8430b36d8d4920fd9023c
Adriana Perez <adriana.perez.garcia@outlook.com>
2018-04-17 18:08:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,9 +2,9 @@
 def position_taken?(board, index)
   if board[index]==" " || board[index]==""|| board[index]==nil
      false
-  elseif  board[index]=="X" || board[index]=="O"
+  elsif  board[index]=="X" || board[index]=="O"
      true
-  #else 
-  #true
+  else 
+     true
   end 
 end   
